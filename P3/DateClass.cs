984e6f47a07a07749218be4ab7c6115ef14437c9
Globlobglob <122725189+Globlobglob@users.noreply.github.com>
2024-02-01 08:59:21 +0200

    Add files via upload

---
+++ P3/DateClass.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P3
+{
+    internal class DateClass
+    {
+       
+        DateTime data;
+        //public DateTime Data { get => data; set => data = value; }
+        public DateClass() { }
+
+        public DateClass(int day,int month,int year)
+        {
+            data = new DateTime(day, month, year);
+        }
+
+        public void NextDay()
+        {
+            Console.WriteLine($"Предыдущий день {data.AddDays(-1).ToString("d")}");
+        }
+        public void PastDay()
+        {
+           Console.WriteLine($"Последующий день {data.AddDays(+1).ToString("d")}");
+        }
+
+        public void DaysCalc()
+        {
+            Console.WriteLine($"До конца месяца {DateTime.DaysInMonth(data.Year, data.Month) - data.Day}");
+        }
+
+       
+      
+
+
+
+    }
+}
---
+++ P3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P3
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+            int year;
+            int month;
+            int day;
+            Console.Write("Year ");
+            year = int.Parse(Console.ReadLine());
+
+            Console.Write("Month ");
+            month = int.Parse(Console.ReadLine());
+
+            Console.Write("Day ");
+            day = int.Parse(Console.ReadLine());
+
+            DateClass date = new DateClass(year, month, day);
+
+
+            date.NextDay();
+            date.PastDay();
+            date.DaysCalc();
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
